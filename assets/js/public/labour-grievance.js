8fe93c031efad8f00f0ca2fafccc8fe9338f43ad
purekid001 <purekid001@gmail.com>
2026-07-27 09:54:06 +0700

    fix grievance timestamp and add QR code

--- assets/js/public/labour-grievance.js
+++ assets/js/public/labour-grievance.js
@@ -468,12 +468,17 @@ function parseDisplayDate(value) {
 }
 
 function getBangkokSubmittedAt(date = new Date()) {
-  return new Intl.DateTimeFormat('th-TH', {
+  const parts = new Intl.DateTimeFormat('en-GB', {
     timeZone: 'Asia/Bangkok',
-    dateStyle: 'medium',
-    timeStyle: 'medium',
-    hour12: false,
-  }).format(date);
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hourCycle: 'h23',
+  }).formatToParts(date);
+  const values = Object.fromEntries(parts.map(part => [part.type, part.value]));
+  return `${values.day}/${values.month}/${values.year} ${values.hour}:${values.minute}`;
 }
 
 function setFieldError(key, message) {
